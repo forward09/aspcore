a628c12e8695c125edc0a99cc724fe1e5ffd4787
a <a@T01907-01>
2016-05-30 13:57:32 +0800

    Add project files.

---
+++ src/NetCoreProduct/Controllers/TodoController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreProduct.Models;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NetCoreProduct.Controllers
+{
+    [Route("api/[controller]")]
+    public class TodoController : Controller
+    {
+        public ITodoRepository ToDoItems;
+
+        public TodoController(ITodoRepository todoItems)
+        {
+            ToDoItems = todoItems;
+        }
+
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAll()
+        {
+            ToDoItems.Add(new TodoItem
+            {
+                Name = "Winfred Xu"
+            });
+
+            return ToDoItems.GetAll();
+        }
+
+
+        // GET api/values/5
+        [HttpGet("{id}", Name ="GetTodo")]
+        public IActionResult GetById(string id)
+        {
+            var item = ToDoItems.Find(id);
+
+            if(item == null)
+            {
+                return NotFound();
+            }
+
+            return new ObjectResult(item);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] TodoItem item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+            ToDoItems.Add(item);
+
+            return CreatedAtRoute("GetToDo", new { controller = "Todo", id = item.Key }, item);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(string id, [FromBody] TodoItem item)
+        {
+            if (item == null || item.Key != id)
+            {
+                return BadRequest();
+            }
+
+            var todo = ToDoItems.Find(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            ToDoItems.Update(item);
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(string id)
+        {
+            ToDoItems.Remove(id);
+        }
+    }
+}
---
+++ src/NetCoreProduct/Models/TodoRepository.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreProduct.Models
+{
+    public class TodoRepository : ITodoRepository
+    {
+        static ConcurrentDictionary<string, TodoItem> todoItems =
+          new ConcurrentDictionary<string, TodoItem>();
+
+
+        public TodoRepository()
+        {
+            Add(new TodoItem
+            {
+                Name = "Item1"
+            });
+        }
+
+        public void Add(TodoItem item)
+        {
+            item.Key = Guid.NewGuid().ToString();
+            todoItems[item.Key] = item;
+        }
+
+        public TodoItem Find(string key)
+        {
+            TodoItem item;
+            todoItems.TryGetValue(key, out item);
+            return item;
+        }
+
+        public IEnumerable<TodoItem> GetAll()
+        {
+            return todoItems.Values;
+        }
+
+        public TodoItem Remove(string key)
+        {
+            TodoItem item;
+
+            todoItems.TryGetValue(key, out item);
+            todoItems.TryRemove(key, out item);
+
+            return item;
+        }
+
+        public void Update(TodoItem item)
+        {
+            todoItems[item.Key] = item;
+        }
+    }
+}
